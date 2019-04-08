2630192bf872cb150412362b1a4db4fba7b6fb2c
Cody Larson <cllarson16@gmail.com>
2019-04-08 17:41:59 -0400

    view defined

--- index.php
+++ index.php
@@ -12,8 +12,12 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function () {
-   echo '<h1>Howdy!</h1>';
+
+    $view = new View();
+    echo $view->render('views/home.html');
 });
+
+
 
 //Run Fat-Free
 $f3->run();
